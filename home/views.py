65fb17b01ebc7490e248fd30751e3b00c3a6c3e6
Abishek0165225 <abishekshah012345@gmail.com>
2021-12-14 16:17:01 +0545

    folder

--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import View
 
-# Create your views here.
+class BaseView(View):
+    views = {}
+
+
+
+
+
+class Homeview(BaseView):
+    def get(self,request):
+
+
+
+
+        return render(request,'index.html',self.views)
